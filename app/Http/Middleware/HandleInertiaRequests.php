56f72a35bbd2ccbc07612ae6ecb95b3f3479bae9
zulazriz <zulazrizulkarnain@gmail.com>
2025-06-11 14:33:21 +0800

    Fixed bugs

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,15 +39,17 @@ class HandleInertiaRequests extends Middleware
     {
         [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
 
+        $user = $request->user()?->load('role');
+
         return [
             ...parent::share($request),
             'name' => config('app.name'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
-                'user' => $request->user(),
-                // 'user' => $user, Code lama ni
+                // 'user' => $request->user(),
+                'user' => $user
             ],
-            'ziggy' => fn (): array => [
+            'ziggy' => fn(): array => [
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
